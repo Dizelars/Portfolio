ea45bd79d5cedc5add95bde6686752a850a431b1
Arseny <ars_escrime@mail.ru>
2021-04-11 18:49:13 +0300

    Portfolio

--- dist/pulse/js/script.js
+++ dist/pulse/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/svg/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/svg/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/svg/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/svg/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
